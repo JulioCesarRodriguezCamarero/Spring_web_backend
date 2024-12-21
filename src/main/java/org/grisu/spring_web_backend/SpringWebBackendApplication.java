a3edf1d3b0d22f3c11a54e0695361799c4b364a4
Julius Kaesar <j3dianas@gmail.com>
2024-12-21 12:50:55 +0100

    commit inicio

---
+++ src/main/java/org/grisu/spring_web_backend/SpringWebBackendApplication.java
@@ -0,0 +1,13 @@
+package org.grisu.spring_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grisu/spring_web_backend/SpringWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grisu.spring_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
